f88e202b95cb9b8e3f4d3d79768d16add574c0d3
Jieun Choi <cje172@naver.com>
2022-06-13 17:33:16 +0900

    브루트 포스

--- BOJ/2798 블랙잭.cpp
+++ BOJ/2798 블랙잭.cpp
@@ -12,11 +12,11 @@ int main()
 		cin >> arr[i];
 
 	for (int i = 0; i < n; i++)
-  {
+  	{
 		for (int j = i + 1; j < n; j++)
-    {
+    		{
 			for (int k = j + 1; k < n; k++)
-      {
+      			{
 				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > result)
 					result = arr[i] + arr[j] + arr[k];
 			}
