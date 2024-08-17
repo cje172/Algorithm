4f4f2e7a3a7505a6e4cbca054f1eedaa783fff9d
Jieun Choi <cje172@naver.com>
2024-08-17 14:54:28 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 7.49 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -2,11 +2,10 @@ class Solution {
     public String solution(String my_string, int n) {
         StringBuilder answer = new StringBuilder();
         
-        for (int i = 0; i < my_string.length(); i++) {
-            for (int j = 0; j < n; j++) {
-                answer.append(my_string.charAt(i));
-            }
+        for (char c: my_string.toCharArray()) {
+            answer.append((c + "").repeat(n));
         }
+        
         return answer.toString();
     }
 }
