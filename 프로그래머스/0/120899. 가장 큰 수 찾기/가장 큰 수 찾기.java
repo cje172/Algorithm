7dbc70bfd695921c1b5a7c492b3e0997c81d515f
Jieun Choi <cje172@naver.com>
2024-08-21 23:23:27 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] solution(int[] array) {
-        int maxValue = array[0];
-        int idx = 0;
+        int[] answer = new int[2];
+        
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > maxValue) {
-                maxValue = array[i];
-                idx = i;
+            if (array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
             }
         }
         
-        int[] answer = {maxValue, idx};
         return answer;
     }
 }
